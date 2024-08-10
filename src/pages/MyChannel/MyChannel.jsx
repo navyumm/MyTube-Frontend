8d164f116a4791241f5f4618ec9935bc0c28522d
navyumm <navyum12@gmail.com>
2024-08-10 22:40:23 +0530

    add videoList and MyChannel

--- src/pages/MyChannel/MyChannel.jsx
+++ src/pages/MyChannel/MyChannel.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
-import { Channel } from "../components/index";
-import {useDispatch, useSelector} from "react-redux"
-import { userChannelProfile } from "../store/userSlice";
+import { Channel } from "../../components/index";
+import { useDispatch, useSelector } from "react-redux"
+import { userChannelProfile } from "../../store/userSlice";
 
 function MyChannel() {
     const dispatch = useDispatch();
     const channel = useSelector((state) => state.auth?.userData);
-    const profile =  useSelector((state) => state.user?.profileData);
+    const profile = useSelector((state) => state.user?.profileData);
 
     useEffect(() => {
         if (channel) {
@@ -16,15 +16,15 @@ function MyChannel() {
 
     return (
         <>
-
-            <Channel 
+            <Channel
                 username={channel?.username}
                 coverImage={profile?.coverImage.url}
                 avatar={profile?.avatar.url}
                 subscribedCount={profile?.channelsSubscribedToCount || 0}
                 fullName={profile?.fullName}
                 subscribersCount={profile?.subscribersCount || 0}
-            />
+            >
+            </Channel>
         </>
     );
 }
